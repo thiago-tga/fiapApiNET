9c3c9874c77e2fb2972fa15ca04bc25292531888
Felipe.Sales <felipe.sales@linx.com.br>
2024-06-27 23:02:07 -0300

    Tentativa testes

--- Fiap.Web.Alunos.Tests/AgendamentoControllerTest.cs
+++ Fiap.Web.Alunos.Tests/AgendamentoControllerTest.cs
@@ -1,17 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Xunit;
 using Fiap.Web.Alunos.Controllers;
 using Fiap.Web.Alunos.Data;
 using Fiap.Web.Alunos.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace Fiap.Web.Alunos.Tests.Controllers
-
+namespace Fiap.Web.Alunos.Tests.AgendamentoControllerTest
 {
-    public class AgendamentoControllerTests
+    public class AgendamentoControllerTest
     {
         [Fact]
         public void IndexDoisAgendamentos_ReturnsViewResultDoisAgendamentos()
@@ -21,15 +20,17 @@ namespace Fiap.Web.Alunos.Tests.Controllers
                 .UseInMemoryDatabase(databaseName: "FiapDb")
                 .Options;
 
+            // Preparar dados de exemplo no contexto em memória
             using (var context = new DatabaseContext(options))
             {
                 context.Agendamentos.AddRange(
-                    new AgendamentoModel { Endereco = "EndereÃ§o 1", Data = DateTime.Now, Cliente = "Cliente 1" },
-                    new AgendamentoModel { Endereco = "EndereÃ§o 2", Data = DateTime.Now, Cliente = "Cliente 2" }
+                    new AgendamentoModel { Endereco = "Endereço 1", Data = DateTime.Now, Cliente = "Cliente 1" },
+                    new AgendamentoModel { Endereco = "Endereço 2", Data = DateTime.Now, Cliente = "Cliente 2" }
                 );
                 context.SaveChanges();
             }
 
+            // Act
             using (var context = new DatabaseContext(options))
             {
                 var controller = new AgendamentoController(context);
@@ -39,9 +40,9 @@ namespace Fiap.Web.Alunos.Tests.Controllers
 
                 // Assert
                 var viewResult = Assert.IsType<ViewResult>(result);
-                Assert.Equal(200, viewResult.StatusCode); // Verifica se o status code Ã© 200
+                Assert.Equal(200, viewResult.StatusCode); // Verifica se o status code é 200
                 var model = Assert.IsAssignableFrom<IEnumerable<AgendamentoModel>>(viewResult.ViewData.Model);
-                Assert.Equal(2, model.Count());
+                Assert.Equal(2, model.Count()); // Verifica se há exatamente 2 agendamentos retornados
             }
         }
     }
